e4a79e029bfcb0ab69050a1e7edab81a44d912ee
sunlei <1255193087@qq.com>
2024-03-18 16:01:49 +0800

    init

--- global/setting.go
+++
@@ -1,9 +0,0 @@
-package global
-
-import "apple/pkg/setting"
-
-var (
-	ServerSetting   *setting.ServerSettingS
-	AppSetting      *setting.AppSettings
-	DatabaseSetting *setting.DatabaseSettingS
-)
--- main.go
+++ main.go
@@ -1,74 +1,7 @@
 package main
 
-import (
-	"apple/global"
-	"apple/internal/model"
-	"apple/internal/routers"
-	"apple/pkg/setting"
-	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
-	"time"
-)
-
-func init() {
-	var err error
-	err = setupSetting()
-	if err != nil {
-		log.Fatalf("init config failed, err:%v\n", err)
-	}
-
-	err = setupDBEngine()
-	if err != nil {
-		log.Fatalf("init DB failed, err:%v\n", err)
-	}
-
-}
-
-func setupSetting() error {
-	setting, err := setting.NewSetting()
-	if err != nil {
-		return err
-	}
-	err = setting.ReadSection("Server", &global.ServerSetting)
-	if err != nil {
-		return err
-	}
-
-	err = setting.ReadSection("App", &global.AppSetting)
-	if err != nil {
-		return err
-	}
-
-	err = setting.ReadSection("Database", &global.DatabaseSetting)
-	if err != nil {
-		return err
-	}
-
-	global.ServerSetting.ReadTimeout *= time.Second
-	global.ServerSetting.WriterTimeout *= time.Second
-	return nil
-}
-
-func setupDBEngine() error {
-	var err error
-	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
+import "fmt"
 
 func main() {
-	gin.SetMode(global.ServerSetting.RunMode)
-	router := routers.NewRouter()
-	s := &http.Server{
-		Addr:           ":" + global.ServerSetting.HttpPort,
-		Handler:        router,
-		ReadTimeout:    global.ServerSetting.ReadTimeout,
-		WriteTimeout:   global.ServerSetting.WriterTimeout,
-		MaxHeaderBytes: 1 << 20,
-	}
-	s.ListenAndServe()
+	fmt.Println("Hello Apple")
 }
